fc3484e5df8ff78448496e6a7a3d41705e88cd66
Lev <lev.kulik@computools.com>
2019-10-07 13:20:27 +0300

    fix routing

--- project_src/src/Controller/DefaultController.php
+++ project_src/src/Controller/DefaultController.php
@@ -7,17 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-/*
+/**
  * @Route("/")
  */
 class DefaultController extends AbstractController
 {
-    /*
-     * @Route("/", name="default_name")
+    /**
+     * @Route("/", name="default_index")
      */
     public function index()
     {
-        return new JsonRresponse([
+        return new JsonResponse([
             'action' => 'index',
             'time' => time(),
         ]);
