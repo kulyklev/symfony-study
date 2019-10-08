4a3a9d67e696a1df77a8d083364c4ce2ed3bc3d8
Lev <kulyklev12@gmail.com>
2019-10-08 11:57:48 +0300

    lesson 42

--- project_src/src/DataFixtures/AppFixtures.php
+++ project_src/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,14 +11,36 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
+
+    public function loadBlogPosts(ObjectManager $manager)
+    {
+        $user = $this->getReference('user_admin');
+
         $blogPost = new BlogPost();
-        $blogPost->setAuthor('Test author');
+        $blogPost->setAuthor($user);
         $blogPost->setTitle('Test blog post');
         $blogPost->setContent('Some test content for test blog post');
         $blogPost->setPublished(new \DateTime() );
         $blogPost->setSlug('test-blog-post');
-        $manager->persist($blogPost);
 
+        $manager->persist($blogPost);
+        $manager->flush();
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('New test user');
+        $user->setEmail('admin@blog.com');
+        $user->setName('admin admin');
+        $user->setPassword('123123123');
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
